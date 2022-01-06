eb7653940cdff42a3255676bfe3b214ac418a822
takwa <blgeyatatou@gmail.com>
2022-01-06 19:50:40 +0100

    first App

--- server/index.js
+++ server/index.js
@@ -5,10 +5,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require('path')
 
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
+
+
+
 mongoose.connect(process.env.MONGO_DB_URI);
 mongoose.connection.on("connected", () => {
   console.log("DB Connected");
