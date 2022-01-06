7be458119adc1b0105a0bd07b18a2841a5e622d5
takwa <blgeyatatou@gmail.com>
2022-01-06 18:57:18 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require('path')
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(process.env.MONGO_DB_URI);
+mongoose.connection.on("connected", () => {
+  console.log("DB Connected");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("DB Connection failed with - ", err);
+});
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const testRoutes = require("./routes/test.routes")
+app.use('/posts', testRoutes)
+app.use( express.static( "./dist" ) );
+app.use( "*", ( req, res ) => {
+  res.sendFile( path.resolve("dist", "index.html" ) );
+} );
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+console.log(`server is running at port ${port}`);
+});
